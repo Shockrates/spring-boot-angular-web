547870921ef6380124c85d4a2b9b86cf411c50fc
unknown <Sokratis@SOKRATIS.EPAL2016.LOCAL>
2020-07-08 16:15:38 +0300

    Made Corrections

--- angularclient/src/app/app.module.ts
+++ angularclient/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserLisyComponent } from './user-list/user-lisy/user-lisy.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserLisyComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule
